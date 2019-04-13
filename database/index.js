5edf08a9a74fd8e3f0ace705477503d961d5ba91
Daniel Su <danielsu@protonmail.ch>
2019-04-12 20:59:11 -0700

    database fix

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/opentable');
+mongoose.connect('mongodb://localhost/reservation');
 
 const reservationSchema = new mongoose.Schema({
   restaurantID: {
