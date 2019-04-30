7786de90f72756816a818abd80e3051bd2804f6d
charles-spinosa <charles.spinosa@gmail.com>
2019-04-29 17:06:06 -0700

    [ ops ] add loader.io verification file + path

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const parser = require('body-parser');
 const cors = require('cors');
 const router = require('./router.js');
+const path = require('path');
 
 const server = express();
 
@@ -13,6 +14,12 @@ server.use(cors());
 
 server.use('/', express.static(`${__dirname}/../public`));
 server.use('/api', router);
+server.use(
+  '/loaderio-a6fb5510aac0d78cf6d1abe23cec0c56/',
+  express.static(
+    path.resolve(__dirname, '../loaderio-a6fb5510aac0d78cf6d1abe23cec0c56')
+  )
+);
 
 const port = process.env.PORT || 3002;
 
