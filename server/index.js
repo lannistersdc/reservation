fa2b88995ee396c7c8bdc817068af0cc2487a39e
charles-spinosa <charles.spinosa@gmail.com>
2019-04-29 17:27:34 -0700

    [ ops ] loader.io route testing

--- server/index.js
+++ server/index.js
@@ -16,10 +16,13 @@ server.use('/', express.static(`${__dirname}/../public`));
 server.use('/api', router);
 server.use(
   '/loaderio-829903ea44142be827d190595de1caaf/',
-  (req,res) => {console.log('im in here'); res.status(200).end('found it')}
+  (req, res) => {
+    console.log('im in here');
+    res.status(200).end('found it');
+  }
   // express.static(
   //   path.resolve(__dirname, '../loaderio-829903ea44142be827d190595de1caaf.txt')
-  )
+  // )
 );
 
 const port = process.env.PORT || 3002;
