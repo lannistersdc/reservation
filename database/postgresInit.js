e29016b0051ba0c4dde4b42c0bc8e202e68eea6f
charles-spinosa <charles.spinosa@gmail.com>
2019-05-02 09:23:22 -0700

    [ ops ] update DB IP

--- database/postgresInit.js
+++ database/postgresInit.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const pool = new pg.Pool({
   user: 'sdcuser2',
-  host: '3.14.152.83',
+  host: '18.224.25.5',
   database: 'reservation',
   password: 'testing123',
   port: 5432
